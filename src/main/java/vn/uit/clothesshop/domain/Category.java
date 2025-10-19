65d4b1843125ec5cc3c9e0f297a6c4b070504c08
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-10-19 20:02:57 +0700

    Format files.

--- src/main/java/vn/uit/clothesshop/domain/Category.java
+++ src/main/java/vn/uit/clothesshop/domain/Category.java
@@ -4,50 +4,56 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name="CATEGORY")
 public class Category {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
     private String image;
     private int amountOfProduct;
+
     public int getId() {
         return id;
     }
+
     public int getAmountOfProduct() {
         return this.amountOfProduct;
-    } 
-    public void setAmountOfProduct(int amountOfProduct) {
-        this.amountOfProduct= amountOfProduct;
     }
+
+    public void setAmountOfProduct(int amountOfProduct) {
+        this.amountOfProduct = amountOfProduct;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getImage() {
         return image;
     }
+
     public void setImage(String image) {
         this.image = image;
     }
-    public Category() {
-    }
+
     public Category(int id, String name, String image, int amountOfProduct) {
         this.id = id;
         this.name = name;
         this.image = image;
-        this.amountOfProduct=amountOfProduct;
+        this.amountOfProduct = amountOfProduct;
     }
 
-    
+    Category() {
+    }
 }
