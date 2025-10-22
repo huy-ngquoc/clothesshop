a6691257c8f30274d02132d9a48eb1266b70601a
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-10-23 05:20:16 +0700

    Change type of category id to "long".

--- src/main/java/vn/uit/clothesshop/repository/CategoryRepository.java
+++ src/main/java/vn/uit/clothesshop/repository/CategoryRepository.java
@@ -1,11 +1,33 @@
 package vn.uit.clothesshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import vn.uit.clothesshop.domain.entity.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            update Category c
+            set c.amountOfProduct = (c.amountOfProduct - :amount)
+            where (c.id = :id)
+                and (c.amountOfProduct >= :amount)
+            """)
+    int decreaseAmountOfProduct(
+            @Param("id") final Long id,
+            @Param("amount") final int amount);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            update Category c
+            set c.amountOfProduct = (c.amountOfProduct + :amount)
+            where (c.id = :id)
+            """)
+    int increaseAmountOfProduct(
+            @Param("id") final Long id,
+            @Param("amount") final int amount);
 }
