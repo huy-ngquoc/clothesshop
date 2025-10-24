d55267f3502a72182944dadeedaf059ac549f792
BinhUIT <22520128@gm.uit.edu.vn>
2025-10-24 21:04:42 +0700

    Logic cho loadUserByUsername

--- src/main/java/vn/uit/clothesshop/config/SecurityConfiguration.java
+++ src/main/java/vn/uit/clothesshop/config/SecurityConfiguration.java
@@ -2,14 +2,37 @@ package vn.uit.clothesshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import vn.uit.clothesshop.service.CustomUserDetailsService;
+import vn.uit.clothesshop.service.UserService;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
     public static final int BCRYPT_ENCODE_LENGTH = 60;
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
 
     @Bean
     PasswordEncoder passwordEncoder() {
