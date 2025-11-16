039ba77e3042f0ad37d61d762fdfa57f7522d4f6
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-11-16 10:22:48 +0700

    Fix order repo

--- src/main/java/vn/uit/clothesshop/feature/order/infra/jpa/repository/OrderRepository.java
+++ src/main/java/vn/uit/clothesshop/feature/order/infra/jpa/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package vn.uit.clothesshop.feature.order.infra.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import vn.uit.clothesshop.feature.order.domain.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 }
