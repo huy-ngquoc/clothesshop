aa5b5bbbf17654ae4a1b07c1d9a5df310d4f0e68
huy-ngquoc <22520565@gm.uit.edu.vn>
2025-11-26 08:29:10 +0700

    Add Bean AuthenticationManager.

--- src/main/java/vn/uit/clothesshop/config/security/SecurityConfig.java
+++ src/main/java/vn/uit/clothesshop/config/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package vn.uit.clothesshop.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -57,6 +59,12 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
